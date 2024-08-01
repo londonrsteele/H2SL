151214e273fd0ffdd3b22a97dacb4fc48f06fdc0
londonrsteele <londonrsteele@gmail.com>
2024-08-01 15:56:03 -0500

    basic window functionality

---
+++ INPUT.py
@@ -0,0 +1,63 @@
+import sys
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtGui import QPainter
+from PySide6.QtWidgets import (QApplication, QFormLayout, QHeaderView,
+                               QHBoxLayout, QLineEdit, QMainWindow,
+                               QPushButton, QTableWidget, QTableWidgetItem,
+                               QVBoxLayout, QWidget)
+from PySide6.QtCharts import QChartView, QPieSeries, QChart
+
+class Widget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.items = 0
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, widget):
+        super().__init__()
+        self.setWindowTitle("Helldivers II Stats Logger")
+
+        # Menu bar
+        self.menu = self.menuBar()
+        
+        # File menu
+        self.file_menu = self.menu.addMenu("File")
+        # File > Exit QAction
+        exit_action = self.file_menu.addAction("Exit", self.close)
+        exit_action.setShortcut("Ctrl+Q")
+
+        # Data menu
+        self.data_menu = self.menu.addMenu("Data")
+        # Data > Add Data menu
+        self.data_add_data_menu = self.data_menu.addMenu("Add Data")
+        # Data > Add Data > Career Data QAction
+        career_data_action = self.data_add_data_menu.addAction("Career Data", self.close)
+        # Data > Add Data > EOM Data QAction
+        EOM_data_action = self.data_add_data_menu.addAction("EOM Data", self.close) 
+        # Data > Load Data menu
+        self.data_load_data_menu = self.data_menu.addMenu("Load Data")
+        ################################################################
+        # 
+        # Add functionality here: List of Save/Load files in dropdown (10 most recent)
+        #
+        ################################################################
+        # Data > Load Data > Demo Data QAction
+        demo_data_action = self.data_load_data_menu.addAction("Demo Data", self.close)
+
+        self.setCentralWidget(widget)
+
+
+if __name__ == "__main__":
+    # Qt Application
+    app = QApplication(sys.argv)
+    # QWidget
+    widget = Widget()
+    # QMainWindow using QWidget as central widget
+    window = MainWindow(widget)
+    window.resize(800, 600)
+    window.show()
+
+    # Execute application
+    sys.exit(app.exec())
---
+++ README.md
@@ -0,0 +1,23 @@
+# Helldivers II Stats Logger
+
+An app created by LostGamma13
+
+## Installation
+
+
+## Quick Start
+
+
+
+
+
+
+
+
+## Versions
+PySide - 6.7.2 (Aug 01, 2024)
+QtCore - 6.7.2 (Aug 01, 2024)
+
+## License
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
