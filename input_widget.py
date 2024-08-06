c0ae1d484126bc570e367177998bfccd6f0af775
londonrsteele <londonrsteele@gmail.com>
2024-08-06 13:49:51 -0500

    added CAR input form

--- input_widget.py
+++ input_widget.py
@@ -45,10 +45,58 @@ class CAR_Input_Widget(QWidget):
         self.form_layout = QFormLayout()
 
         # Create Entry Boxes
+        self.CAR_date = QDateEdit()
+        self.CAR_logtime = QTimeEdit()
         self.CAR_enemy_kills = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_terminid_kills = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_automaton_kills = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_friendly_kills = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_grenade_kills = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_melee_kills = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_eagle_kills = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_deaths = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_shots_fired = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_shots_hit = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_orbitals_used = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_def_strats_used = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_eagle_strats_used = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_supply_strats_used = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_reinforce_strats_used = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_total_strats_used = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_successful_extractions = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_objectives_completed = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_missions_played = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_missions_won = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_inmisison_time = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_samples_collected = QSpinBox(minimum=0, maximum=1000000000)
+        self.CAR_total_XP_earned = QSpinBox(minimum=0, maximum=1000000000)
 
         # Add Entry Boxes to layout
+        self.form_layout.addRow(("Career Log Date: "), self.CAR_date)
+        self.form_layout.addRow(("Career Log Time: "), self.CAR_logtime)
         self.form_layout.addRow(("Enemy Kills: "), self.CAR_enemy_kills)
+        self.form_layout.addRow(("Terminid Kills: "), self.CAR_terminid_kills)
+        self.form_layout.addRow(("Automaton Kills: "), self.CAR_automaton_kills)
+        self.form_layout.addRow(("Friendly Kills: "), self.CAR_friendly_kills)
+        self.form_layout.addRow(("Grenade Kills: "), self.CAR_grenade_kills)
+        self.form_layout.addRow(("Melee Kills: "), self.CAR_melee_kills)
+        self.form_layout.addRow(("Eagle Kills: "), self.CAR_eagle_kills)
+        self.form_layout.addRow(("Deaths: "), self.CAR_deaths)
+        self.form_layout.addRow(("Shots Fired: "), self.CAR_shots_fired)
+        self.form_layout.addRow(("Shots Hit: "), self.CAR_shots_hit)
+        self.form_layout.addRow(("Orbitals Used: "), self.CAR_orbitals_used)
+        self.form_layout.addRow(("Defensive Stratagems Used: "), self.CAR_def_strats_used)
+        self.form_layout.addRow(("Eagle Stratagems Used: "), self.CAR_eagle_strats_used)
+        self.form_layout.addRow(("Supply Stratagems Used: "), self.CAR_supply_strats_used)
+        self.form_layout.addRow(("Reinforce Stratagems Used: "), self.CAR_reinforce_strats_used)
+        self.form_layout.addRow(("Total Stratagems Used: "), self.CAR_total_strats_used)
+        self.form_layout.addRow(("Successful Extractions: "), self.CAR_successful_extractions)
+        self.form_layout.addRow(("Objectives Completed: "), self.CAR_objectives_completed)
+        self.form_layout.addRow(("Missions Played: "), self.CAR_missions_played)
+        self.form_layout.addRow(("Missions Won: "), self.CAR_missions_won)
+        self.form_layout.addRow(("In-Mission Time: "), self.CAR_inmisison_time)
+        self.form_layout.addRow(("Samples Collected: "), self.CAR_samples_collected)
+        self.form_layout.addRow(("Total XP Earned: "), self.CAR_total_XP_earned)
 
         # Return groupbox to grid layout
         self.CAR_groupbox.setLayout(self.form_layout)
@@ -58,17 +106,45 @@ class CAR_Input_Widget(QWidget):
     # CAR_Input_Widget member function: CAR_form_save
     ################################################################
     def CAR_form_save(self):
+        # Convert DateTimes to filename-friendly formats
+        car_day_formatted = self.CAR_date.date().toString("MMMM_d_yyyy")
+        car_time_formatted = self.CAR_logtime.time().toString("H_m")
+
         # Put data from form into Pandas DataFrame
-        CAR_data = {"CAR_enemy_kills":self.CAR_enemy_kills.text()
+        CAR_data = {"CAR_date":self.CAR_date.text(),
+                    "CAR_logtime":self.CAR_logtime.text(),
+                    "CAR_enemy_kills":self.CAR_enemy_kills.text(),
+                    "CAR_terminid_kills":self.CAR_terminid_kills.text(),
+                    "CAR_automaton_kills":self.CAR_automaton_kills.text(),
+                    "CAR_friendly_kills":self.CAR_friendly_kills.text(),
+                    "CAR_grenade_kills":self.CAR_grenade_kills.text(),
+                    "CAR_melee_kills":self.CAR_melee_kills.text(),
+                    "CAR_eagle_kills":self.CAR_eagle_kills.text(),
+                    "CAR_deaths":self.CAR_deaths.text(),
+                    "CAR_shots_fired":self.CAR_shots_fired.text(),
+                    "CAR_shots_hit":self.CAR_shots_hit.text(),
+                    "CAR_orbitals_used":self.CAR_orbitals_used.text(),
+                    "CAR_def_strats_used":self.CAR_def_strats_used.text(),
+                    "CAR_eagle_strats_used":self.CAR_eagle_strats_used.text(),
+                    "CAR_supply_strats_used":self.CAR_supply_strats_used.text(),
+                    "CAR_reinforce_strats_used":self.CAR_reinforce_strats_used.text(),
+                    "CAR_total_strats_used":self.CAR_total_strats_used.text(),
+                    "CAR_successful_extractions":self.CAR_successful_extractions.text(),
+                    "CAR_objectives_completed":self.CAR_objectives_completed.text(),
+                    "CAR_missions_played":self.CAR_missions_played.text(),
+                    "CAR_missions_won":self.CAR_missions_won.text(),
+                    "CAR_inmission_time":self.CAR_inmisison_time.text(),
+                    "CAR_samples_collected":self.CAR_samples_collected.text(),
+                    "CAR_total_XP_earned":self.CAR_total_XP_earned.text()
                     }
         CAR_df = pd.DataFrame(data=CAR_data, index=[0])
 
         # Create csv file
-        file_path = "./save_files/career_savefile.csv"
+        file_path = "./save_files/career_savefile_" + car_day_formatted + "_" + car_time_formatted + ".csv"
         try:
             with open(file_path, "x") as file:
                 file.close()
-
+                
             # Write DataFrame to file
             CAR_df.to_csv(file_path, index=False)
 
