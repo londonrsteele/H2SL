facd2cf056832d8c345f451c8ef43636a2bf403a
londonrsteele <londonrsteele@gmail.com>
2024-08-09 18:27:50 -0500

    minor tweaks

--- assets/stylesheets.py
+++ assets/stylesheets.py
@@ -9,6 +9,7 @@ QPushButton {
     font: bold 20px;
     min-height: 50px;
     max-width: 200px;
+    padding: 5px;
 }
 """
 
@@ -17,6 +18,21 @@ QGroupBox {
     border: none;
 }
 """
+
+Log_Time_Buttons = """
+QPushButton {
+    background-color: #FCFCFC;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 6px;
+    border-color: black;
+    font: bold 12px;
+    min-height: 15px;
+    max-width: 200px;
+    padding: 5px;
+}
+"""
+
 
 Title = """
 QLabel {
