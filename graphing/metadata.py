941d016557d3d29adf88f9bdced3f85fad14986a
londonrsteele <londonrsteele@gmail.com>
2024-08-08 22:36:14 -0500

    created metadata file

---
+++ graphing/metadata.py
@@ -0,0 +1,37 @@
+list_of_strats = [
+    # Kills
+    "Total Kills", # curated
+    "Enemy Kills",
+    "Terminid Kills",
+    "Automaton Kills",
+    "Grenade Kills",
+    "Melee Kills", 
+    "Eagle Kills",
+    "Team Kills",
+    "Shot Kills",
+    # Accuracy
+    "Accuracy",
+    "Shots Fired",
+    "Shots Hit",
+    # Survivor
+    "Deaths",
+    "Stims Used",
+    # Stratagems
+    "Total Stratagems Used",
+    "Defensive Stratagems Used",
+    "Eagle Stratagems Used",
+    "Supply Stratagems Used",
+    "Reinforce Stratagems Used",
+    # Other
+    "Orbitals Used",
+    "Times Reinforcing",
+    # Game
+    "Successful Extractions",
+    "Samples Collected",
+    "Objectives Completed",
+    "Missions Played",
+    "Missions Won",
+    "In-Misison Time",
+    "Distance Traveled",
+    "Total XP Earned"
+]
